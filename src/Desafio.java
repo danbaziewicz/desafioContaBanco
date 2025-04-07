168f49778e41536145b5fb858691f796d5a1725a
Danylo Baziewicz <danxbazi@gmail.com>
2025-04-07 19:37:20 -0300

    Início do projeto de desafio.

---
+++ src/Desafio.java
@@ -0,0 +1,16 @@
+public class Desafio {
+    public static void main(String[] args) {
+        String nome = "Peter Parker";
+        String tipoConta = "Conta Corrente";
+        double saldo = 1000;
+        double limite = 1000;
+        boolean status = true;
+        System.out.println("*****************************");
+        System.out.println("Nome: " + nome);
+        System.out.println("Tipo de conta: " + tipoConta);
+        System.out.println("Saldo: " + saldo);
+        System.out.println("Limite: " + limite);
+        System.out.println("Status: " + status);
+        System.out.println("*****************************");
+    }
+}
